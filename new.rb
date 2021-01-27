4dff50af4c6cdd5b33c11481ff3fbe72fc58a9d4
Jamie Cajka <jamiecajka@gmail.com>
2021-01-27 10:44:13 -0600

    filled out first 3x3 box

--- new.rb
+++ new.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 #make board
 board = Array.new
-
  9.times do
    line = Array.new
    while line.length < 9
@@ -12,29 +11,73 @@ board = Array.new
    board << line
  end
 
-a = 0
-b = 0
+ a = 0
+ b = 0
+ box = Array.new
+def make_a_box(a,b,board,box)  #make box array to test 3x3 square
+ 3.times do
+   line_test = Array.new
+   3.times do
+     line_test << board[a][b]
+     b += 1
+   end
+   box << line_test
+   a += 1
+   b = b - 3
+ end
+ return box
+end
+
+column =  Array.new
+def make_a_column(a,b,board,column)  #make column array to test column
+  9.times do
+    column << board[b][a]
+    b += 1
+  end
+  return column
+end
+
+available = Array.new
+def make_available_number_list(available)  #create array of available numbers
+  num = 1
+  9.times do
+    available << num
+    num += 1
+  end
+  return available.shuffle!
+end
+
 #make row array to test row
 row = board[a]
-#make column array to test column
-column =  Array.new
-c = b
-9.times do
-  column << board[c][a]
-  c += 1
+
+make_a_box(a,b,board,box)
+make_a_column(a,b,board,column)
+make_available_number_list(available)
+
+line = 0
+col = 0
+count = 8
+while available.count > 0  #fill in first box
+  num = available[0]
+  if !box.include?(num) && !column.include?(num) && !row.include?(num)
+    board[line][col] = num
+    box[line][col] = num
+    available = available.slice!(1,count)
+    count -= 1
+  else
+    go_to_end = available.shift()
+    available << go_to_end
+  end
+  if col == 2
+    line += 1
+    col = 0
+    row = board[line]
+  else
+    col += 1
+  end
+  column = []
+  make_a_column(col,0,board,column)
+  #binding.pry
 end
-#make box array to test 3x3 square
-box = Array.new
-e = b
-3.times do
-  line_test = Array.new
-  d = a
-  3.times do
-    line_test << board[d][e]
-    e += 1
-  end #do
-  box << line_test
-  d += 1
-  e = b
-end #top do
-binding.pry
+
+  binding.pry
